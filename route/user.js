60c85ade18ef1cb6c04831e3c8914041046dd192
OTTooo <passten512@gmail.com>
2017-05-23 21:04:25 +0800

    ...

--- route/user.js
+++ route/user.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-// 登录
+// 注册
 router.get('/register',function(req,res) {
   res.render('register',{
     title: '注册'
   });
 });
 
-// 注册
+// 注册提交
+router.post('/register',function(req,res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password1 = req.body.password1;
+  var password2 = req.body.password2;
+
+  // 验证
+  req.checkBody('username','Name is require').notEmpty();
+
+  var errors = req.validationErrors();
+  if (errors) {
+    console.log('YES');
+  } else {
+    console.log('NO');
+  }
+});
+
+// 登录
 router.get('/login',function(req,res) {
   res.render('login', {
     title: '登录'
   });
 });
+
 
 module.exports = router
